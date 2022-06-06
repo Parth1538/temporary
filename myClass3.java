7ba2f6f1bfcc21fe38268353efc771cc317baa2b
Parth1538 <patelparth1538@gmail.com>
2022-06-06 12:16:57 -0400

    only java test codes

---
+++ myClass3.java
@@ -0,0 +1,25 @@
+class myClass3{
+public static void main(String[] args){
+	int i = 3;
+	int j = 4;
+	int k = i+j;
+	int y = 10;
+	
+	if (k>5){
+	System.out.println("The total sums of i and j: ");
+	System.out.println(k);
+	}
+	else {
+		{
+			System.out.println("THe sum is :");
+			System.out.println(k);
+		}
+	}
+	
+	for(i=0; i <= 5; i++){
+		System.out.println("THis is the value of X:" +i);
+		y = y-2;
+		System.out.println("This is the value of y" + y);
+	}
+}
+}
